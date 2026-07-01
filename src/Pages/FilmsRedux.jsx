1753a1ebf719e4bab22b3b2e5d2a9c222ebcc0ca
Youssef Ibrahim <youssefalhares2@gmail.com>
2026-07-01 23:27:32 +0300

    Fix import case for MovieDetailsModal component

--- src/Pages/FilmsRedux.jsx
+++ src/Pages/FilmsRedux.jsx
@@ -2,7 +2,7 @@ import React, { useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { getAllMovies } from '../Api/moviesApi'
 import Card from '../components/card'
-import MovieDetailsModal from '../components/movieDetailsModal'
+import MovieDetailsModal from '../components/MovieDetailsModal'
 import './css.css'
 function FilmsRedux() {
 
